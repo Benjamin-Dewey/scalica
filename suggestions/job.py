236351a942efd4bb1bc2b7db1f1da1b13345e25d
Benjamin-Dewey <bd1170@nyu.edu>
2018-12-06 02:50:45 -0500

    create the suggestions job which for now simply echoes the current following relationships back into a key value store

---
+++ suggestions/job.py
@@ -0,0 +1,35 @@
+# TODO: make this job run on some kind of schedule
+# and actually trigger a mapreduce job and write the
+# results into pickledb
+
+import mysql.connector
+import pickledb
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="appserver",
+  passwd="foobarzoot",
+  database="scalica"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM auth_user")
+
+users = mycursor.fetchall()
+
+following_dict = {}
+
+for user in users:
+  user_id = user[0]
+  mycursor.execute("SELECT * FROM micro_following WHERE follower_id = " + str(user_id))
+  followings = mycursor.fetchall()
+  following_dict[user_id] = [following[2] for following in followings]
+  
+# TODO: start a map reduce job and replace the
+# right side of the equal sign with the results
+suggestions_dict = following_dict
+
+db = pickledb.load('suggestions.db', False)
+for key, value in suggestions_dict.iteritems(): db.set(str(key), value)
+db.dump()
