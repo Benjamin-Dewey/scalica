f49598231d970ffb927073443d6c0b8a79a282c2
Benjamin Dewey <benjamin.m.dewey@gmail.com>
2018-12-06 19:53:48 -0500

    show users on django console

--- web/scalica/micro/admin.py
+++ web/scalica/micro/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from .models import Post, Following, ReverseFollowing
 
 # Register your models here.
+admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(Following)
 admin.site.register(ReverseFollowing)
